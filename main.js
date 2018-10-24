0bae00716f0dbd4a94122e29af9141a1fd09e4e4
Taylor Fletcher <taylorwfletcher@gmail.com>
2018-10-24 00:37:55 -0600

    added star wars section, changed color scheme, and more

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { films } from '/assets/films.js'
-//import { people } from '/assets/people.js'
+// import { people } from '/assets/people.js'
 //import { starships } from '/assets/starships.js'
+//import { planets } from '/assets/planets.js'
 
 
 let filmList = document.querySelector("#film-list")
@@ -12,6 +13,27 @@ films.forEach((film) => {
   console.log(film.title)
 })
 
+
+let crawlList = document.querySelector("#crawl-list")
+
+films.forEach((film) => {
+  let listItem3 = document.createElement('li')
+  listItem3.textContent = film.opening_crawl
+  crawlList.appendChild(listItem3)
+  console.log(film.opening_crawl)
+})
+
+
+
+
+// let personList = document.querySelector("#person-list")
+
+// people.forEach((people) => {
+//   let listItem2 = document.createElement('li')
+//   listItem2.textContent = people.name
+//   personList.appendChild(listItem2)
+//   console.log(people.name)
+// })
 //console.log(films[2])
 
 
