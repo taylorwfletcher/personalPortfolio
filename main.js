06733db95b88faf4b76b86e133556409255d5639
Taylor Fletcher <taylorwfletcher@gmail.com>
2018-10-28 23:22:45 -0600

    added 'scroll to top' button

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ import { films } from '/assets/films.js'
 //import { planets } from '/assets/planets.js'
 
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 let filmList = document.querySelector("#film-list")
 
 films.forEach((film) => {
