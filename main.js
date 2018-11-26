80a6f420bf463badea80b4b2e9682f6a71ef7b07
Taylor Fletcher <taylorwfletcher@gmail.com>
2018-11-25 23:57:38 -0700

    added wrapper, cleaned up code, minor aesthetic changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
